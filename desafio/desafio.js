e0930575ae9a2920b9b3e6fd9d7c3eff7773ffa6
AlineAlves10 <alves.aline1177@gmail.com>
2025-04-12 12:45:24 -0300

    desafio preta lab

---
+++ desafio/desafio.js
@@ -0,0 +1,38 @@
+/* Contagem de Medalhas
+Crie um programa que receba o número de medalhas de ouro, prata e bronze de um país nas Olimpíadas e calcule o total de medalhas. Quando escrever 'sair', o programa deverá encerrar e mostrar na tela o ranking de medalhas no formato:
+
+
+#Ranking de medalhas:
+#Brasil: 7 medalhas
+#França: 6 medalhas
+#Argentina: 3 medalhas
+
+*/
+
+const paises = [];
+const medalhas = [];
+const rankeando = []; 
+
+while (true) {
+    let pais = prompt('Digite o nome de um pais ou sair para terminar:');
+
+    if (pais.toLocaleLowerCase() == 'sair') {
+        break;
+    }
+    paises.push(pais);
+
+    let medalhas_ouro = Number(prompt(`Quantas medalhas de Ouro o ${pais} tem:`));
+    let medalhas_prata = Number(prompt(`Quantas medalhas de Prata o ${pais} tem:`));
+    let medalhas_bronze = Number(prompt(`Quantas medalhas de Bronze o ${pais} tem:`));
+
+    let calculo = medalhas_ouro + medalhas_prata + medalhas_bronze;
+    medalhas.push(calculo);
+}
+
+for (let i = 0; i < paises.length; i++) {
+    rankeando.push({ pais: paises[i], totalMedalhas: medalhas[i] });
+}
+
+
+
+alert()
